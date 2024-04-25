bcc1edf26ca12006db18ee604009c68b0d357005
marleneh456 <82187174+marleneh456@users.noreply.github.com>
2024-04-24 19:35:04 -0500

    Update registration.php

--- Learn Linx/registration.php
+++ Learn Linx/registration.php
@@ -23,12 +23,11 @@ if (!$conn) {
 if (isset($_POST['but_submit'])) {
     $uname = mysqli_real_escape_string($conn, $_POST['txt_uname']);
     $name = mysqli_real_escape_string($conn, $_POST['name']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']); // Capture email from the form and sanitize
     $password = mysqli_real_escape_string($conn, $_POST['txt_pwd']);
-    $password = password_hash($password, PASSWORD_DEFAULT); // Hash password
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = password_hash($password, PASSWORD_DEFAULT); // Hash the password before storing it
 
-    // SQL query to insert the user data into the database
-    $query = "INSERT INTO `users` (username, name, email, password) VALUES ('$uname', '$name', '$email', '$password')";
+    $query = "INSERT INTO `users` (username, name, password, email) VALUES ('$uname', '$name', '$password', '$email')";
     $result = mysqli_query($conn, $query);
     if ($result) {
         echo "<div class='form'>
@@ -38,7 +37,7 @@ if (isset($_POST['but_submit'])) {
     } else {
         echo "<div class='form'>
               <h3>Required fields are missing.</h3><br/>
-              <p class='link'>Click here to <a href='registration.php'>registration</a> again.</p>
+              <p class='link'>Click here to <a href='registration.php'>register</a> again.</p>
               </div>";
     }
 } else {
@@ -46,36 +45,26 @@ if (isset($_POST['but_submit'])) {
     <form class="form" action="" method="post">
     <div id="div_reg">
         <h1 class="registration-title">Registration</h1>
-        
         <div>
-        <label for="txt_uname">Username</label>
-        <br><br>
-        <input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Username" />
-        <br>
+            <label for="txt_uname">Username</label>
+            <input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Username" />
         </div>
-        
         <div>
-        <label for="name">Name</label>
-        <br><br>
-        <input type="text" class="login-input" id="name" name="name" placeholder="Name">
-        <br><br>
-
-        <label for="email">Email</label>
-        <br><br>
-        <input type="email" class="textbox" id="email" name="email" placeholder="Email">
-        <br><br>
-
-        <label for="txt_pwd">Password</label>
-        <br><br>
-        <input type="password" class="textbox" id="txt_pwd" name="txt_pwd" placeholder="Password"/>
-        <br>
+            <label for="name">Name</label>
+            <input type="text" class="login-input" name="name" placeholder="Name">
         </div>
-        
         <div>
-        <br>
-        <input type="submit" value="Submit" name="but_submit" id="but_submit" />
+            <label for="email">Email</label>
+            <input type="email" class="textbox" id="email" name="email" placeholder="Email" required />
         </div>
-        <p class="link">Already have an account? <a href='index.php'>Login here</a></p>
+        <div>
+            <label for="txt_pwd">Password</label>
+            <input type="password" class="textbox" id="txt_pwd" name="txt_pwd" placeholder="Password"/>
+        </div>
+        <div>
+            <input type="submit" value="Submit" name="but_submit" id="but_submit" />
+        </div>
+        <p class="link">Already have an account? <a href="index.php">Login here</a></p>
     </div>
     </form>
 <?php

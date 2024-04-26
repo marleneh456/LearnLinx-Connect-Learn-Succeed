973fab0ee9d87d6916bce90cdf1ceae9b34239b6
marleneh456 <82187174+marleneh456@users.noreply.github.com>
2024-04-26 17:40:07 -0500

    Update users.sql

--- Learn-Linx/users.sql
+++ Learn-Linx/users.sql
@@ -10,12 +10,12 @@ CREATE TABLE Users (
   username VARCHAR(30),
   name VARCHAR(30),
   password VARCHAR(100),
-  email VARCHAR(100) -- Adding email column
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  email VARCHAR(100)
+);
 
 INSERT INTO Users (username, name, password, email) 
 
 VALUES('marleneh', 'Marlene Habib', 'Pass12345', 'MarleneH@example.com'),
-	( 'johnnydoes', 'John Doe', 'Pass12345', 'JohnDoe@example.com'),
-      ( 'lillyPaddie', 'Lilly', 'Pass12345', 'LillyPad@example.com'),
+	  ( 'johnnydoes', 'John Doe', 'Pass12345', 'JohnDoe@example.com'),
+      ( 'lillyPaddie', 'Lilly Pad', 'Pass12345', 'LillyPad@example.com'),
       ( 'AlexMarching', 'Alex March', 'Pass12345', 'AlexM@example.com');

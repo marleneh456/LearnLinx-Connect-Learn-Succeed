0f08273d68ede318517d98b823dfae52a2a193ec
Marlene Habib <82187174+marleneh456@users.noreply.github.com>
2024-04-28 18:47:32 -0500

    Update registration.php

--- Learn-Linx/registration.php
+++ Learn-Linx/registration.php
@@ -1,7 +1,6 @@
-<!DOCTYPE html>
 <html>
 <head>
-    <title>Registration</title>
+    <title>Reset Password</title>
     <link href="login.css" rel="stylesheet" type="text/css">
 	<link href='https://fonts.googleapis.com/css?family=Overpass' rel='stylesheet'>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -10,8 +9,8 @@
 <?php
 session_start();
 
-$servername = "localhost"; 
-$username = "root"; 
+$servername = "localhost";
+$username = "root";
 $password = ""; 
 $database = "learn_linx"; 
 
@@ -23,82 +22,76 @@ $conn = new mysqli($servername, $username, $password, $database);
 		die("Connection failed: " . $conn->connect_error);
 	}
 
-if (isset($_POST['but_submit'])) {
-    $uname = htmlspecialchars($_POST['txt_uname']);
-    $name = htmlspecialchars($_POST['name']);
-    $password = htmlspecialchars($_POST['txt_pwd']);
-    $email = htmlspecialchars($_POST['email']);
-    $password = preg_replace("/[^a-zA-Z0-9]/", "", $password);
+if (isset($_POST['but_reset'])) {
+    $uname_email = htmlspecialchars($_POST['txt_uname_email']);
+    $new_password = htmlspecialchars($_POST['txt_new_pwd']);
 
-    $query = "INSERT INTO Users (username, name, password, email) VALUES ('$uname', '$name', '$password', '$email')";
-    $result = mysqli_query($conn, $query);
+    $new_password = preg_replace("/[^a-zA-Z0-9]/", "", $new_password); // Store only the numbers from the password
+
+    $sql_query = "UPDATE Users SET password = '$new_password' WHERE username = '$uname_email' OR email = '$uname_email'";
+    $result = mysqli_query($conn, $sql_query);
+
     if ($result) {
+        // Password reset was successful
         echo "<div class='form'>
-              <h3>You are registered successfully.</h3><br/>
-              <p class='link'>Click here to <a href='index.php'>Login</a></p>
+              <h1>Password has been reset successfully.</h1><br>
+              <p class='page-link'>Click here to <a href='index.php'>Login</a></p>
               </div>";
     } else {
+        // Password reset failed
         echo "<div class='form'>
-              <h3>Required fields are missing.</h3><br/>
-              <p class='link'>Click here to <a href='registration.php'>register</a> again.</p>
+              <h1>Failed to reset password. Please check the username or email provided.</h1><br>
+              <p class='page-link'>Click here to <a href='reset_password.php'>try again</a>.</p>
               </div>";
     }
 } else {
 ?>
+
 <div class="background">
     <img src="bg.jpg" alt="sample image" class="blur">
 </div>
-<div class="reg">
-    <form action="" method="post">
-    <div id="div_reg">
-        <h1 class="registration-title">Registration</h1>
-		<div>
-            <label for="email">Email</label>
+
+<div class="forgot">
+    <form method="post" action="reset_password.php">
+        <div id="div_forgot">
+		<h1 class="forgot-title">Reset Password</h1>
+            <div>
+                <label for="txt_uname_email">Username or Email</label>
+				<br>
+				<br>
+                <input type="text" class="textbox" id="txt_uname_email" name="txt_uname_email" placeholder="Email or Username"/>
+            </div>
 			<br>
-			<br>
-            <input type="email" class="textbox" id="email" name="email" placeholder="Email" required />
-        </div>
-		<div>
-            <label for="name">Name</label>
-			<br>
-			<br>
-            <input type="text" class="login-input" name="name" placeholder="Name">
-        </div>
-        <div>
-            <label for="txt_uname">Username</label>
-			<br>
-			<br>
-            <input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Username" />
-        </div>
-        <div>
-            <label for="txt_pwd">Password</label>
-			<br>
-			<br>
-            <input type="password" id="txt_pwd" name="txt_pwd" placeholder="Password" required/>
-			<br>
-			<br>
-			<input type="checkbox" id="show-password" onclick="togglePasswordVisibility()">Show Password
-        </div>
-		<br>
-		<div id="password-validation">
+            <div>
+                <label for="txt_new_pwd">New Password</label>
+				<br>
+				<br>
+                <input type="password" class="textbox" id="txt_pwd" name="txt_new_pwd" placeholder="New Password"/>
+				<br>
+				<br>
+				<input type="checkbox" id="show-password" onclick="togglePasswordVisibility()">Show Password
+            </div>
+			 <div id="password-validation">
                 <h3>Password must contain the following:</h3>
                 <p id="letter" class="invalid">A <b>lowercase</b> letter</p>
                 <p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
                 <p id="number" class="invalid">A <b>number</b></p>
 				<p id="length" class="invalid">At least <b>8 characters</b></p>
             </div>
+			<br>
             <div>
-                <input type="submit" value="Submit" name="but_submit" id="but_submit"/>
+                <input type="submit" value="Reset" name="but_reset" id="but_reset" />
             </div>
-             <p class="link">Already have an account? <a href="index.php">Login here</a></p>
-        </div>
+			
+			<div>
+			<p class="link">Go back to the <a href="index.php">Login</a></p>
+			</div>
+	</div>		
     </form>
 <?php
 }
-?>
+?>	
 </div>
-
 <script src="password-validation.js"></script>
-
 </body>
 </html>

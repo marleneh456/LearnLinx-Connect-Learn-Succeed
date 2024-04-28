5eb7a657179bb9cb7017be638ba879a1cc890671
Marlene Habib <82187174+marleneh456@users.noreply.github.com>
2024-04-28 09:27:43 -0500

    Update script.js

--- Learn-Linx/script.js
+++ Learn-Linx/script.js
@@ -1,219 +1,180 @@
-$(document).ready(function () {
-    $('#html').click(function () {
-        showVideoAndQuiz('HTML', 'https://www.youtube.com/embed/qz0aGYrrlhU?si=uPa8lFjlrd2-nIRw');
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('html').addEventListener('click', function () {
+        showVideoAndQuiz('HTML', 'https://www.youtube.com/embed/qz0aGYrrlhU');
     });
-
-    $('#html2').click(function () {
+	
+	document.getElementById('html2').addEventListener('click', function () {
         showVideoAndQuiz('HTML 2', 'https://www.youtube.com/embed/FQdaUv95mR8');
     });
-
-    $('#html3').click(function () {
+	
+	document.getElementById('html3').addEventListener('click', function () {
         showVideoAndQuiz('HTML 3', 'https://www.youtube.com/embed/kUMe1FH4CHE');
     });
-
-    $('#html4').click(function () {
+	
+	document.getElementById('html4').addEventListener('click', function () {
         showVideoAndQuiz('HTML 4', 'https://www.youtube.com/embed/916GWv2Qs08');
     });
 
-    $('#css').click(function () {
-        showVideoAndQuiz('CSS', 'https://www.youtube.com/embed/1PnVor36_40?si=Uyv1sGlf6RlOBA-s');
+    document.getElementById('css').addEventListener('click', function () {
+        showVideoAndQuiz('CSS', 'https://www.youtube.com/embed/1PnVor36_40');
     });
-
-    $('#css2').click(function () {
+	
+	document.getElementById('css2').addEventListener('click', function () {
         showVideoAndQuiz('CSS 2', 'https://www.youtube.com/embed/wRNinF7YQqQ');
     });
-
-    $('#css3').click(function () {
+	
+	document.getElementById('css3').addEventListener('click', function () {
         showVideoAndQuiz('CSS 3', 'https://www.youtube.com/embed/1Rs2ND1ryYc?si=zWCsBZImRUCbKBC7');
     });
-
-    $('#css4').click(function () {
+	
+	document.getElementById('css4').addEventListener('click', function () {
         showVideoAndQuiz('CSS 4', 'https://www.youtube.com/embed/yfoY53QXEnI');
     });
 
-    $('#javascript').click(function () {
-        showVideoAndQuiz('JavaScript', 'https://www.youtube.com/embed/W6NZfCO5SIk?si=W-6Q25ao6Sphctbo');
+    document.getElementById('javascript').addEventListener('click', function () {
+        showVideoAndQuiz('JavaScript', 'https://www.youtube.com/embed/W6NZfCO5SIk');
     });
-
-    $('#javascript2').click(function () {
+	
+	document.getElementById('javascript2').addEventListener('click', function () {
         showVideoAndQuiz('JavaScript 2', 'https://www.youtube.com/embed/hdI2bqOjy3c');
     });
-
-    $('#javascript3').click(function () {
+	
+	document.getElementById('javascript3').addEventListener('click', function () {
         showVideoAndQuiz('JavaScript 3', 'https://www.youtube.com/embed/lfmg-EJ8gm4');
     });
-
-    $('#javascript4').click(function () {
+	
+	document.getElementById('javascript4').addEventListener('click', function () {
         showVideoAndQuiz('JavaScript 4', 'https://www.youtube.com/embed/E3XxeE7NF30');
     });
 });
 
-
-
-
 function showVideoAndQuiz(subject, link) {
-    const videoContainer = document.getElementById('videoContainer');
-    videoContainer.innerHTML = `<iframe width="560" height="315" src="${link}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    var videoContainer = document.getElementById('videoContainer');
+    videoContainer.innerHTML = '<iframe width="560" height="315" src="' + link + '" frameborder="0" allowfullscreen></iframe>';
 
-    // Find the quiz corresponding to the subject
-    const quizData = getQuizDataBySubject(subject);
+    var quizData = getQuizDataBySubject(subject);
     displayQuiz(quizData);
 }
 
-// Function to retrieve quiz data based on the subject
 function getQuizDataBySubject(subject) {
-    switch (subject) {
-        case 'HTML':
-            return htmlQuizData;
-        case 'HTML 2':
-            return html2QuizData;
-        case 'CSS':
-            return cssQuizData;
-        case 'JavaScript':
-            return jsQuizData;
-        default:
-            return [];
-    }
+    var quizzes = {
+        'HTML': htmlQuizData,
+        'HTML 2': html2QuizData,
+        'CSS': cssQuizData,
+        'JavaScript': jsQuizData
+    };
+    return quizzes[subject] || [];
 }
 
-// Function to display quiz
 function displayQuiz(quizData) {
-    const quizContainer = document.getElementById('quiz');
-    const resultContainer = document.getElementById('result');
-    const submitButton = document.getElementById('submit');
-    const retryButton = document.getElementById('retry');
-    const showAnswerButton = document.getElementById('showAnswer');
+    var quizContainer = document.getElementById('quiz');
+    var resultContainer = document.getElementById('result');
+    quizContainer.innerHTML = '';
+    resultContainer.innerHTML = '';
 
-    let currentQuestion = 0;
-    let score = 0;
-    let incorrectAnswers = [];
+    for (var i = 0; i < quizData.length; i++) {
+        var question = quizData[i];
+        var questionElement = document.createElement('div');
+        questionElement.classList.add('question'); // Add class for question styling
+        questionElement.textContent = question.question;
 
-    // Function to shuffle array
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    }
+        var optionsContainer = document.createElement('div');
+        optionsContainer.classList.add('options'); // Add class for options styling
 
-    // Function to display question
-    function displayQuestion() {
-        const questionData = quizData[currentQuestion];
+        for (var j = 0; j < question.options.length; j++) {
+            var option = question.options[j];
+            var optionLabel = document.createElement('label');
+            optionLabel.classList.add('option'); // Add class for option styling
 
-        const questionElement = document.createElement('div');
-        questionElement.className = 'question';
-        questionElement.innerHTML = questionData.question;
-
-        const optionsElement = document.createElement('div');
-        optionsElement.className = 'options';
-
-        const shuffledOptions = [...questionData.options];
-        shuffleArray(shuffledOptions);
-
-        for (let i = 0; i < shuffledOptions.length; i++) {
-            const option = document.createElement('label');
-            option.className = 'option';
-
-            const radio = document.createElement('input');
-            radio.type = 'radio';
-            radio.name = 'quiz';
-            radio.value = shuffledOptions[i];
-
-            const optionText = document.createTextNode(shuffledOptions[i]);
-
-            option.appendChild(radio);
-            option.appendChild(optionText);
-            optionsElement.appendChild(option);
+            var optionInput = document.createElement('input');
+            optionInput.type = 'radio';
+            optionInput.name = 'question' + i;
+            optionInput.value = option;
+            optionLabel.appendChild(optionInput);
+            optionLabel.append(' ' + option);
+            optionsContainer.appendChild(optionLabel);
         }
 
-        quizContainer.innerHTML = '';
         quizContainer.appendChild(questionElement);
-        quizContainer.appendChild(optionsElement);
+        quizContainer.appendChild(optionsContainer);
     }
 
-    // Function to check answer
-    function checkAnswer() {
-        const selectedOption = document.querySelector('input[name="quiz"]:checked');
-        if (selectedOption) {
-            const answer = selectedOption.value;
-            if (answer === quizData[currentQuestion].answer) {
-                score++;
-            } else {
-                incorrectAnswers.push({
-                    question: quizData[currentQuestion].question,
-                    incorrectAnswer: answer,
-                    correctAnswer: quizData[currentQuestion].answer,
-                });
-            }
-            currentQuestion++;
-            selectedOption.checked = false;
-            if (currentQuestion < quizData.length) {
-                displayQuestion();
-            } else {
-                displayResult();
-            }
-        }
-    }
-
-    // Function to display result
-    function displayResult() {
-        quizContainer.style.display = 'none';
-        submitButton.style.display = 'none';
-        retryButton.style.display = 'inline-block';
-        showAnswerButton.style.display = 'inline-block';
-        resultContainer.innerHTML = `You scored ${score} out of ${quizData.length}!`;
-    }
-
-    // Function to retry quiz
-    function retryQuiz() {
-        currentQuestion = 0;
-        score = 0;
-        incorrectAnswers = [];
-        quizContainer.style.display = 'block';
-        submitButton.style.display = 'inline-block';
-        retryButton.style.display = 'none';
-        showAnswerButton.style.display = 'none';
-        resultContainer.innerHTML = '';
-        displayQuestion();
-    }
-
-    // Function to show answer
-    function showAnswer() {
-        quizContainer.style.display = 'none';
-        submitButton.style.display = 'none';
-        retryButton.style.display = 'inline-block';
-        showAnswerButton.style.display = 'none';
-
-        let incorrectAnswersHtml = '';
-        for (let i = 0; i < incorrectAnswers.length; i++) {
-            incorrectAnswersHtml += `
-                <p>
-                    <strong>Question:</strong> ${incorrectAnswers[i].question}<br>
-                    <strong>Your Answer:</strong> ${incorrectAnswers[i].incorrectAnswer}<br>
-                    <strong>Correct Answer:</strong> ${incorrectAnswers[i].correctAnswer}
-                </p>
-            `;
-        }
-
-        resultContainer.innerHTML = `
-            <p>You scored ${score} out of ${quizData.length}!</p>
-            <p>Incorrect Answers:</p>
-            ${incorrectAnswersHtml}
-        `;
-    }
-
-    submitButton.addEventListener('click', checkAnswer);
-    retryButton.addEventListener('click', retryQuiz);
-    showAnswerButton.addEventListener('click', showAnswer);
-
-    displayQuestion();
+    var submitButton = document.createElement('button');
+    submitButton.textContent = 'Submit Answers';
+    submitButton.id = 'submit-answers';
+    submitButton.classList.add('button'); // Add class for button styling
+    submitButton.onclick = function () {
+        checkAnswers(quizData);
+    };
+    quizContainer.appendChild(submitButton);
 }
+
+function checkAnswers(quizData) {
+    var score = 0;
+    var wrongAnswers = [];
+    for (var i = 0; i < quizData.length; i++) {
+        var selected = document.querySelector('input[name="question' + i + '"]:checked');
+        if (selected && selected.value === quizData[i].answer) {
+            score++;
+        } else {
+            wrongAnswers.push(i); // Save the index of the wrong answer
+        }
+    }
+    var resultContainer = document.getElementById('result');
+    resultContainer.innerHTML = 'You scored ' + score + ' out of ' + quizData.length + '<br>' + '<br>';
+
+    var submitButton = document.getElementById('submit-answers');
+    submitButton.style.display = 'none'; // Hide submit button
+
+    if (wrongAnswers.length > 0) {
+        var showAnswerButton = document.createElement('button');
+        showAnswerButton.textContent = 'Show Answers';
+        showAnswerButton.onclick = function () {
+            showAnswers(wrongAnswers, quizData);
+        };
+        resultContainer.appendChild(showAnswerButton);
+    }
+}
+
+
+function showAnswers(wrongAnswers, quizData) {
+    var resultContainer = document.getElementById('result');
+    resultContainer.innerHTML = '<div>Your incorrect answers:</div>' + '<br>';
+    
+    // Create a div to hold the list of incorrect answers
+    var incorrectAnswersList = document.createElement('div');
+    incorrectAnswersList.id = 'incorrect-answers-list';
+
+    // Fill the incorrect answers list with the wrong answers
+    for (var i = 0; i < wrongAnswers.length; i++) {
+        var questionIndex = wrongAnswers[i];
+        var question = quizData[questionIndex];
+        var selectedAnswer = document.querySelector('input[name="question' + questionIndex + '"]:checked').value;
+        
+        var incorrectAnswerDiv = document.createElement('div');
+        incorrectAnswerDiv.innerHTML = '<strong>Question:</strong> ' + question.question + '<br><strong>Correct Answer:</strong> ' + question.answer + '<br><strong>You Answer:</strong> ' + selectedAnswer;
+        incorrectAnswersList.appendChild(incorrectAnswerDiv);
+        incorrectAnswersList.innerHTML += '<br>';
+    }
+
+    // Append the incorrect answers list to the result container
+    resultContainer.appendChild(incorrectAnswersList);
+
+    // Create and append the retry button to the result container
+    var retryButton = document.createElement('button');
+    retryButton.textContent = 'Retry';
+    retryButton.onclick = function () {
+        displayQuiz(quizData); // Redisplay the quiz
+    };
+    resultContainer.appendChild(retryButton);
+}
+
 
 
 // HTML Quiz Data
 const htmlQuizData = [
     {
-        question: 'What does HTML stand for?',
+        question: '1. What does HTML stand for?',
         options: [
             'HyperText Markup Language',
             'HyperTool Markup Language',
@@ -223,7 +184,7 @@ const htmlQuizData = [
         answer: 'HyperText Markup Language'
     },
     {
-        question: 'Which tool does the instructor recommend for writing HTML and CSS code in the tutorial?',
+        question: '2. Which tool does the instructor recommend for writing HTML and CSS code in the tutorial?',
         options: [
             'Adobe Dreamweaver',
             'Atom',
@@ -233,7 +194,7 @@ const htmlQuizData = [
         answer: 'Visual Studio Code'
     },
     {
-        question: 'What is the purpose of the <strong> tag in HTML?',
+        question: '3. What is the purpose of the <strong> tag in HTML?',
         options: [
             'To change the font color to strong colors like red or blue',
             'To indicate that the text is important and should be bolded',
@@ -243,7 +204,7 @@ const htmlQuizData = [
         answer: 'To indicate that the text is important and should be bolded'
     },
     {
-        question: 'What does CSS stand for?',
+        question: '4. What does CSS stand for?',
         options: [
             'Cascading Style Sheets',
             'Combined Styling Sheets',
@@ -253,7 +214,7 @@ const htmlQuizData = [
         answer: 'Cascading Style Sheets'
     },
     {
-        question: 'Which extension does the instructor use in the tutorial to automatically refresh the web page upon saving the changes?',
+        question: '5. Which extension does the instructor use in the tutorial to automatically refresh the web page upon saving the changes?',
         options: [
             'Prettier Code Formatter',
             'Live Server',
@@ -265,27 +226,27 @@ const htmlQuizData = [
 ];
 const html2QuizData = [
     {
-        question: 'What is HTML an acronym for?',
+        question: '1. What is HTML an acronym for?',
         options: ['Hyper Text Markup Language', 'High Text Markup Language', 'Hyper Tech Markup Language', 'Hyperlink Text Markup Language'],
         answer: 'Hyper Text Markup Language'
     },
     {
-        question: 'According to Kevin, what is the basic building block of the web?',
+        question: '2. According to Kevin, what is the basic building block of the web?',
         options: ['JavaScript', 'CSS', 'HTML', 'SQL'],
         answer: 'HTML'
     },
     {
-        question: 'What does Kevin suggest using to create an HTML website on your computer?',
+        question: '3. What does Kevin suggest using to create an HTML website on your computer?',
         options: ['Notepad', 'Adobe Dreamweaver', 'Microsoft Word', 'Visual Studio Code'],
         answer: 'Notepad'
     },
     {
-        question: 'What browser does Kevin use to open the HTML file?',
+        question: '4. What browser does Kevin use to open the HTML file?',
         options: ['Mozilla Firefox', 'Microsoft Edge', 'Google Chrome', 'Safari'],
         answer: 'Microsoft Edge'
     },
     {
-        question: 'What is the purpose of the <h1> tag as demonstrated by Kevin?',
+        question: '5. What is the purpose of the <h1> tag as demonstrated by Kevin?',
         options: ['To create a link', 'To define a header', 'To insert an image', 'To write a paragraph'],
         answer: 'To define a header'
     }
@@ -294,7 +255,7 @@ const html2QuizData = [
 // CSS Quiz Data
 const cssQuizData = [
     {
-        question: 'What does CSS stand for?',
+        question: '1. What does CSS stand for?',
         options: ['Computer Style Sheets', 'Colorful Style Sheets', 'Cascading Style Sheets', 'Creative Style Sheets'],
         answer: 'Cascading Style Sheets',
     },
@@ -304,7 +265,7 @@ const cssQuizData = [
 // JavaScript Quiz Data
 const jsQuizData = [
     {
-        question: 'Which one of these is a JavaScript package manager?',
+        question: '1. Which one of these is a JavaScript package manager?',
         options: ['Node.js', 'TypeScript', 'npm', 'ESLint'],
         answer: 'npm',
     },
